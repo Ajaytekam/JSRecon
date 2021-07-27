0516a458381f3d9266e44b2cae976c126c110502
Ajay Tekam <x90ajay@gmail.com>
2021-07-27 09:55:04 +0530

    libs added with colored outputs

--- jsrecon.py
+++ jsrecon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3  
 
+from libs.telegramText import NotifyTelegramBot 
+import libs.coloredOP as co
 from pathlib import Path
 import os
 import datetime
@@ -8,35 +10,16 @@ import requests
 import subprocess 
 import argparse 
 import sys
-import configparser
 
 def executeCommand(COMMAND, verbose=False):
     try:
         subprocess.run(COMMAND, shell=True, check=True, text=True)
         if verbose:
-            print("\t[*] Command Executed Successfully.")
+            print("\t"+co.bullets.OK, co.colors.GREEN++"Command Executed Successfully."+co.END)
     except subprocess.CalledProcessError as e:
-        print("\t[-] Error During Command Execution.!!")
+        print("\t"+co.bullets.ERROR, co.colors.BRED++"Error During Command Execution.!!"+co.END)
         print(e.output)
     return 
-
-def NotifyTelegramBot(textMessage):
-    ConfigPath = "/root/notificationConfig.ini"
-    print("[+] Sending notification to telegram bot")
-    config = configparser.RawConfigParser()
-    if os.path.isfile(ConfigPath):
-        config.read(ConfigPath)
-        if config.has_option("telegram","apiToken") and config.has_option("telegram","chatId"): 
-            apiToken = config.get("telegram","apiToken")
-            chatId = config.get("telegram","chatId")
-            send_text = 'https://api.telegram.org/bot'+apiToken+'/sendMessage?chat_id='+chatId+'&parse_mode=Markdown&text='+textMessage
-            response = requests.post(send_text)
-            if response.status_code == 200:
-                print("\t[!] Message Send successfully")
-        else:
-            print("[-] Error : no credentials are setted for Telegram bot (API token and ChatId)")
-    else:
-        print("[-] Error : There is no config file available '/root/notificationConfig.ini'")
 
 def ValideteDomain(domain):
     regex =  "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}"
@@ -54,8 +37,11 @@ def main():
     args = parser.parse_args()
     # Check argument
     if args.url is None:
-        print("JSRecon\tversion0.1")
-        print("Developed by : securebitlabs.com\n")
+        print("############################################")
+        print("# "+co.BOLD+co.colors.GREEN+"JSRecon"+co.END+co.BOLD+" : Javascript Reconnaissance Tool"+co.END)
+        print("# "+co.BOLD+"Developed by : "+co.colors.RED+"securebitlabs.com"+co.END)
+        print("# version : 0.1")
+        print("############################################\n")
         parser.print_help()
         sys.exit()
     ## GLOBAL Vars
@@ -66,7 +52,7 @@ def main():
     if(ValideteDomain(args.url)):
         tDomain = args.url 
     else:
-        print("[-] Invalid Domain: {}".format(args.url))
+        print(co.bullets.ERROR, co.colors.BRED+"Invalid Domain:{}".format(args.url)+co.END)
         sys.exit()
     # get the http protocol 
     try:
@@ -79,7 +65,7 @@ def main():
             Domain = tempD.url
             Domain = re.sub(":80/$", "", Domain)
         except:
-            print("[-] Error : Could not resolve the Http protocol.!!")
+            print(co.bullets.ERROR, co.colors.BRED+" Error : Could not resolve the Http protocol.!!"+co.END)
             sys.exit(1)
     # Sending telegram message
     txtmessage = "JSRecon staretd for domain : {}".format(Domain)
@@ -88,16 +74,16 @@ def main():
     if args.out is not None:
         OPDir = args.out
         if os.path.isdir(OPDir):
-            print("[+] {} already exists...".format(OPDir))
-            print("[+] Adding time-stamp into the directory name as suffix")
+            print(co.bullets.INFO+"{} already exists...".format(OPDir)+co.END)
+            print(co.bullets.INFO+"Adding time-stamp into the directory name as suffix"+co.END)
             Date = str(datetime.datetime.now())
             WORKDIR = re.sub("-|:|\.|\ ", "_", Date)
             OPDir += "_{}".format(WORKDIR)
     else:
         OPDir = "./jsrecon_{}".format(tDomain)
         if os.path.isdir(OPDir):
-            print("[+] {} already exists...".format(OPDir))
-            print("[+] Adding time-stamp into the directory name as suffix")
+            print(co.bullets.INFO+"{} already exists...".format(OPDir)+co.END)
+            print(co.bullets.INFO+"[+] Adding time-stamp into the directory name as suffix"+co.END)
             Date = str(datetime.datetime.now())
             WORKDIR = re.sub("-|:|\.|\ ", "_", Date)
             OPDir += "_{}".format(WORKDIR)
@@ -106,30 +92,30 @@ def main():
     ## Collecting Javascript urls
     os.chdir(OPDir)
     COMMAND = 'gau {} | grep -iE "\.js$" >> tempgau'.format(tDomain)
-    print("[+] Collecting JS urls using gau ")
+    print(co.bullets.CProcess, co.colors.GREEN+"Collecting JS urls using gau"+co.END)
     executeCommand(COMMAND)
     ## Collecting Javascript subjs 
     UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
     COMMAND = 'echo {} | subjs -c 10 -ua "{}" >> tempsubjs'.format(Domain, UserAgent)
-    print("[+] Collecting JS urls using subjs ")
+    print(co.bullets.CProcess, co.colors.GREEN+"Collecting JS urls using subjs"+co.END)
     executeCommand(COMMAND)
     ## Collecting Javascript hakrawler 
     COMMAND = 'echo {} | hakrawler -d 3 -insecure | grep -iE "\.js$" >> temphakrawler'.format(Domain)
-    print("[+] Collecting JS urls using hakrawler ")
+    print(co.bullets.CProcess, co.colors.GREEN+"Collecting JS urls using hakrawler "+co.END)
     executeCommand(COMMAND)
     ## merging all JS files
     COMMAND = 'cat tempgau tempsubjs temphakrawler | sort -u >> jsurls'
-    print("[+] Merging all collected URLs")
+    print(co.bullets.CProcess, co.colors.GREEN+"Merging all collected URLs"+co.END)
     executeCommand(COMMAND)
     ## Get the JS files with status 200
     COMMAND = 'cat jsurls | httpx -silent -mc 200 >> js_200'
-    print("[+] Filtering collected URLs for HTTP Status 200 ")
+    print(co.bullets.CProcess, co.colors.GREEN+"Filtering collected URLs for HTTP Status 200 "+co.END)
     executeCommand(COMMAND)
     # Count Number of JS found
     JSfile = open("js_200", "r").read()
     NoOfJS = JSfile.count("\n") - 1
     if NoOfJS > 0:
-        print("[!] {} Javascript files found".format(NoOfJS))
+        print(co.bullets.INFO+"{} Javascript files found".format(NoOfJS)+co.END)
     # Delete temp files 
     if os.path.isfile("tempgau"):
         os.remove("tempgau")
@@ -140,27 +126,27 @@ def main():
     ##################
     ## Download JS Files 
     if args.download:
-        print("[+] Downloading JS files into local machine")
+        print(co.bullets.CProcess, co.colors.GREEN+"Downloading JS files into local machine"+co.END)
         os.mkdir("JSFiles")
         os.chdir("JSFiles")
         try:
             subprocess.run('cat ../js_200 | parallel -j50 -q curl -O -J -sk', shell=True, text=True)
-            print("\t[*] Files Downloaded Successfully..") 
+            print("\t"+co.bullets.OK, co.colors.CYAN+"Files Downloaded Successfully.."+co.END) 
         except subprocess.CalledProcessError as e:
-            print("There are some problem during downloading of JS Files.")
+            print(co.bullets.ERROR, co.colors.BRED+"There are some problem during downloading of JS Files."+co.END)
             print(e.output)
         os.chdir("..")
     ################## 
     ## Get JS Endpoints  
     COMMAND = 'while read -r jsurl; do echo "[ + ] URL: $jsurl" >> linkfinderResult.txt;linkfinder -d -i $jsurl -o cli >> linkfinderResult.txt; printf "\n\n" >> linkfinderResult.txt; done < js_200'
-    print("[+] Scraping Endpoints with linkfinder ")
+    print(co.bullets.CProcess, co.colors.GREEN+"Scraping Endpoints with linkfinder "+co.END)
     executeCommand(COMMAND)
     ################## 
     ## Get Javascript Secrets   
     COMMAND = 'while read -r jsurl; do secretfinder -i $jsurl -o cli >> secretfinderResults.txt;printf "\n\n" >> secretfinderResults.txt; done < js_200'
     print("[+] Scraping Secrets with secretfinder ")
     executeCommand(COMMAND)
-    print("[+] Javascript Reconnaissance Completed.. ")
+    print(co.bullets.OK, co.colors.CYAN+"Javascript Reconnaissance Completed.."+co.END)
     # Sending telegram message
     txtmessage = "JSRecon Completed for Domain : {}".format(Domain)
     NotifyTelegramBot(txtmessage)
